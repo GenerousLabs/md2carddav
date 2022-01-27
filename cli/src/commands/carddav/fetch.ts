bde64113f43bbbce3cfe4c6b776565484f9d8ae8
Callum Macdonald <git@callum-macdonald.com>
2022-01-27 20:31:37 +0100

    Create CardDAV client. Seems to work.

--- cli/src/commands/carddav/fetch.ts
+++ cli/src/commands/carddav/fetch.ts
@@ -1,4 +1,10 @@
 import { Command, Flags } from "@oclif/core";
+import { createAccount, createDAVClient, fetchAddressBooks } from "tsdav";
+import { getCache, setCache } from "../../services/cache/cache.service";
+
+const serverUrl = process.env.URL as string;
+const username = process.env.USER as string;
+const password = process.env.TOKEN as string;
 
 export default class CarddavFetch extends Command {
   static description = "describe the command here";
@@ -6,21 +12,82 @@ export default class CarddavFetch extends Command {
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: "f" }),
+    url: Flags.string({
+      char: "u",
+      description: "CardDAV URL",
+      // required: true,
+    }),
   };
 
-  static args = [{ name: "file" }];
+  static args = [];
+
+  private async setup() {
+    const cache = getCache();
+
+    this.log("Got cache #xwONmh");
+    this.debug(cache);
+
+    const client = await createDAVClient({
+      serverUrl,
+      authMethod: "Basic",
+      credentials: {
+        username,
+        password,
+      },
+    });
+
+    this.log("Got client #bVoqc1");
+    this.debug(client);
+
+    if (
+      typeof cache.account !== "undefined" &&
+      typeof cache.addressBooks !== "undefined"
+    ) {
+      this.log("Got cached account and addressBooks #AGOahk");
+      return {
+        client,
+        account: cache.account as Awaited<ReturnType<typeof createAccount>>,
+        addressBooks: cache.addressBooks as Awaited<
+          ReturnType<typeof fetchAddressBooks>
+        >,
+      };
+    }
+
+    // NOTE: This can take >40s to complete against a monica instance with ~4k
+    // contacts, need to figure out how to cache the result.
+    const account = await client.createAccount({
+      account: {
+        serverUrl,
+        accountType: "carddav",
+      },
+    });
+
+    this.log("Got account #ToupBB");
+    this.debug(account);
+
+    const addressBooks = await client.fetchAddressBooks({ account });
+
+    this.log("Got address books #kzx54I");
+    this.debug(addressBooks);
+
+    setCache("account", account);
+    setCache("addressBooks", addressBooks);
+
+    return { client, account, addressBooks };
+  }
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(CarddavFetch);
 
-    const name = flags.name ?? "world";
-    this.log(`hello ${name}`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
-    }
+    const { client, account, addressBooks } = await this.setup();
+
+    const vcards = await client.fetchVCards({
+      addressBook: addressBooks[0],
+    });
+
+    this.log("Got vcards #VHvQJe");
+    this.debug(vcards);
+
+    this.log("Running #qcKLqf", args, flags);
   }
 }
