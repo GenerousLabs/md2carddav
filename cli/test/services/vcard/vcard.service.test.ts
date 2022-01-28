29cefca537a5a6ce435d7adf44931f6d79cad6af
Callum Macdonald <git@callum-macdonald.com>
2022-01-28 20:12:30 +0100

    More tests.

--- cli/test/services/vcard/vcard.service.test.ts
+++ cli/test/services/vcard/vcard.service.test.ts
@@ -9,4 +9,34 @@ describe("vcard.service", () => {
       `BEGIN:VCARD\r\nVERSION:4.0\r\nFN:Jane Doe\r\nUID:1\r\nEND:VCARD`
     );
   });
+
+  test.it(
+    "Correctly encodes an array of phone numbers with types #O54AMf",
+    () => {
+      expect(
+        generateVcardFromContact({
+          uid: "1",
+          name: { full: "Jane Doe" },
+          phones: [{ type: "mobile", phone: "+1 123 12345" }],
+        })
+      ).to.equal(
+        `BEGIN:VCARD\r\nVERSION:4.0\r\nFN:Jane Doe\r\nTEL;TYPE=mobile:+1 123 12345\r\nUID:1\r\nEND:VCARD`
+      );
+    }
+  );
+
+  test.it(
+    "Correctly encodes an array of phone numbers as strings #gqnt3J",
+    () => {
+      expect(
+        generateVcardFromContact({
+          uid: "1",
+          name: { full: "Jane Doe" },
+          phones: ["+1 123 12345"],
+        })
+      ).to.equal(
+        `BEGIN:VCARD\r\nVERSION:4.0\r\nFN:Jane Doe\r\nTEL:+1 123 12345\r\nUID:1\r\nEND:VCARD`
+      );
+    }
+  );
 });
