a500f2f9d4d99f8981a100106496a3398d3f7e32
Callum Macdonald <git@callum-macdonald.com>
2022-01-29 12:33:57 +0100

    Wipe seems to work.

--- cli/src/commands/carddav/wipe.ts
+++ cli/src/commands/carddav/wipe.ts
@@ -1,29 +1,62 @@
 import { Command, Flags } from "@oclif/core";
+import { getClientAndAccount } from "../../services/carddav/carddav.service";
+import { getContext } from "../../shared.utils";
 
 export default class CarddavWipe extends Command {
   static description = `delete every contact in an address book
   This command is extremely destructive, it will delete every contact in a given address book. It is highly recommended to run fetch first and take a backup.
-  Why? This command can be useful if you want to delete all contacts and then push again from markdown. Otherwise contacts will never be deleted.`;
+  Why? This command can be useful if you want to delete all contacts and then push again from markdown. Otherwise contacts will never be deleted.
+  Use the list command to list address books, each one must be wiped separately.`;
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
+    "address-book": Flags.string({
+      char: "a",
+      description: "address book to wipe",
+      required: true,
+    }),
     force: Flags.boolean({ char: "f" }),
+    verbose: Flags.boolean({ char: "v" }),
   };
 
-  static args = [{ name: "file" }];
-
   public async run(): Promise<void> {
-    const { args, flags } = await this.parse(CarddavWipe);
+    const { flags } = await this.parse(CarddavWipe);
+    const { force, "address-book": addressBookDisplayName, verbose } = flags;
 
-    if (!flags.force) {
-      this.log("Refusing to run without the --force flag.");
-      return;
+    if (!force) {
+      return this.error("Refusing to run without the --force flag.");
     }
 
-    this.log("list #6NPfxJ", args, flags);
+    const context = await getContext(this);
+
+    const { client, account } = await getClientAndAccount(context);
+
+    const addressBooks = await client.fetchAddressBooks({ account });
+
+    const addressBook = addressBooks.find(
+      ({ displayName }) => displayName === addressBookDisplayName
+    );
+
+    if (typeof addressBook === "undefined") {
+      this.debug("No address book match. #5nfKaD", addressBooks);
+      return this.error(
+        "Invalid address book. Not found on the configured CardDAV server. #4UruD6"
+      );
+    }
+
+    const vCards = await client.fetchVCards({ addressBook });
+    this.debug("Fetched VCards #MQfF1Q", vCards);
+
+    for (const vCard of vCards) {
+      this.debug("About to delete vcard #RehJiY", vCard);
+      // eslint-disable-next-line no-await-in-loop
+      await client.deleteVCard({ vCard });
+      if (verbose) {
+        this.log(`Deleted #L5cPlO ${vCard.url}`);
+      }
+    }
+
+    this.log(`Deleted ${vCards.length} contacts #xFTULL`);
   }
 }
