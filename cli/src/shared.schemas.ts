f06ea77f767e04499065329e607bb5c2d78df118
Callum Macdonald <git@callum-macdonald.com>
2022-01-29 18:06:43 +0100

    Move schema. And bugfix, `refine() => true`.
    Schemas fail if `refine()` doesn't return truthy.

---
+++ cli/src/shared.schemas.ts
@@ -0,0 +1,100 @@
+import { z } from "zod";
+
+export const ContactSchemaBase = z.object({
+  uid: z.string(),
+  company: z.string().optional(),
+  name: z
+    .object({
+      full: z.string().optional(),
+      prefix: z.string().or(z.string().array()).optional(),
+      first: z.string().or(z.string().array()).optional(),
+      middle: z.string().or(z.string().array()).optional(),
+      last: z.string().or(z.string().array()).optional(),
+      suffix: z.string().or(z.string().array()).optional(),
+    })
+    .optional(),
+  photo: z.string().optional(),
+  phones: z
+    .array(
+      z
+        .object({
+          type: z.string(),
+          phone: z.string(),
+        })
+        .or(z.string())
+    )
+    .optional(),
+  emails: z
+    .array(
+      z
+        .object({
+          type: z.string(),
+          email: z.string(),
+        })
+        .or(z.string())
+    )
+    .optional(),
+  addresses: z
+    .array(
+      z
+        .object({
+          type: z.string().optional(),
+          line1: z.string().optional(),
+          line2: z.string().optional(),
+          city: z.string().optional(),
+          state: z.string().optional(),
+          postcode: z.string().optional(),
+          country: z.string().optional(),
+        })
+        .refine((obj) => {
+          // Ensure that there is not an empty address
+          if (
+            typeof obj.line1 === "undefined" &&
+            typeof obj.line2 === "undefined" &&
+            typeof obj.city === "undefined" &&
+            typeof obj.postcode === "undefined" &&
+            typeof obj.state === "undefined" &&
+            typeof obj.country === "undefined"
+          ) {
+            return false;
+          }
+
+          return true;
+        })
+    )
+    .optional(),
+  urls: z
+    .array(
+      z.string().or(
+        z.object({
+          type: z.string(),
+          url: z.string(),
+        })
+      )
+    )
+    .optional(),
+  desc: z.string().optional(),
+});
+
+export const ContactSchema = ContactSchemaBase.refine((obj) => {
+  // Ensure that the object has at least one name
+  if (typeof obj.name !== "undefined") {
+    if (typeof obj.name.full === "string" && obj.name.full.length > 0) {
+      return true;
+    }
+
+    if (typeof obj.name.first === "string" && obj.name.first.length > 0) {
+      return true;
+    }
+
+    if (typeof obj.name.last === "string" && obj.name.last.length > 0) {
+      return true;
+    }
+  }
+
+  if (typeof obj.company === "string" && obj.company.length > 0) {
+    return true;
+  }
+
+  return false;
+});
