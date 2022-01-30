afc4f7e5e0fbae7d334ceb52853b886127b55eaa
Callum Macdonald <git@callum-macdonald.com>
2022-01-30 17:37:08 +0100

    Improved Contact schema.
    One of full name, first name, last name, company name, or email address must be present and non empty (excluding whitespace).

--- cli/src/shared.schemas.ts
+++ cli/src/shared.schemas.ts
@@ -76,31 +76,48 @@ export const ContactSchemaBase = z.object({
   desc: z.string().optional(),
 });
 
+/**
+ * A VCF file must have an `FN:` (full name) attribute. But it doesn't
+ * need to be an actual name. It could be an email address, a company
+ * name, whatever. We require at least one of full name, first name, last name,
+ * company name, or email address. If a full name is not present, we can fill it
+ * in with one of those values instead.
+ */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const ContactSchema = ContactSchemaBase.refine((obj) => {
-  // VCards don't require a name, they can be valid with only an email
-  return true;
-
-  /*
   // Ensure that the object has at least one name
   if (typeof obj.name !== "undefined") {
-    if (typeof obj.name.full === "string" && obj.name.full.length > 0) {
+    if (typeof obj.name.full === "string" && obj.name.full.trim().length > 0) {
       return true;
     }
 
-    if (typeof obj.name.first === "string" && obj.name.first.length > 0) {
+    if (
+      typeof obj.name.first === "string" &&
+      obj.name.first.trim().length > 0
+    ) {
       return true;
     }
 
-    if (typeof obj.name.last === "string" && obj.name.last.length > 0) {
+    if (typeof obj.name.last === "string" && obj.name.last.trim().length > 0) {
       return true;
     }
   }
 
-  if (typeof obj.company === "string" && obj.company.length > 0) {
+  if (typeof obj.company === "string" && obj.company.trim().length > 0) {
     return true;
   }
 
+  if (typeof obj.emails !== "undefined" && obj.emails.length > 0) {
+    for (const email of obj.emails) {
+      if (typeof email === "string") {
+        if (email.trim().length > 0) {
+          return true;
+        }
+      } else if (email.email.trim().length > 0) {
+        return true;
+      }
+    }
+  }
+
   return false;
-  */
 });
